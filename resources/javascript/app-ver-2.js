3b83e7be247ccc470d5fddfbd17bdd1cec0e9a14
awcoetzer <83278375+awcoetzer@users.noreply.github.com>
2023-05-09 16:01:01 +0300

    fix: update js comments
    Added javascript comments for understanding of whats going on.

--- resources/javascript/app-ver-2.js
+++ resources/javascript/app-ver-2.js
@@ -1,20 +1,27 @@
 'use strict';
-
+// selecting the range input content.
 const rangeSliderEl = document.querySelector('.range-slider');
 const rangeSliderTextEl = document.querySelector('.range-slider--text');
+
+// selecting all toogle buttons content.
 const toggleBtnsEl = document.querySelectorAll('.toggle--input');
 const toggleFillsEl = document.querySelectorAll('.toggle--fill')
 const toggleTextsEl = document.querySelectorAll('.toggle-text')
 
+// selecting the generator button.
 const generateStretchBtnEl = document.querySelector('.btn--pg-stretch');
 const displayStretchTextsEl = document.querySelectorAll('.display-text--stretch');
 
+// selecting the copy button.
 const btnsCopyEl = document.querySelectorAll('.btn--copy');
 const copyTextEl = document.querySelectorAll('.copy-text');
 
+// setting the defaults for some variables use later.
 let sliderValue, uppercase, number, symbol;
-let passCopy = []
 
+/*
+  below holds different arrays housing either, lovercase, uppercase, numbers or symbols seperately.
+*/
 const lowercaseArr = [
   "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
 ];
@@ -31,7 +38,10 @@ const symbolsArr = [
   "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?"
 ]
 
-
+/*
+  the first function here is to initialize the
+  page to a default state.
+*/
 const init = function () {
   rangeSliderTextEl.textContent = rangeSliderEl.value
   rangeSliderTextEl.style.color = 'var(--clr-dt-grey)';
@@ -43,15 +53,31 @@ const init = function () {
   
 }
 
+// here I am calling the init function to set the page.
 init()
 
-console.log(sliderValue)
-
+/*
+  the function below has a parameter slider to compensate for
+  the length chosen by the user.
+*/
 const randomPassStretch = function (slider) {
   let pass = ''
 
+  /*
+    defining a new variable that only pertains to the
+    scope of this function. We also set the function equal 
+    to lowercase as a default set of characters.
+
+    this so the generate button can on initiate generate 
+    at least lowercase letters.
+  */
   let allCharacters = lowercaseArr;
 
+  /*
+    below is just a sequence of statements checking if
+    the above varaibles are true and then adding them to 
+    this newly created variable above.
+  */
   if (uppercase) {
     allCharacters = allCharacters.concat(uppercaseArr);
   }
@@ -64,6 +90,11 @@ const randomPassStretch = function (slider) {
     allCharacters = allCharacters.concat(symbolsArr)
   }
 
+  /*
+    loops the amount set by the slider, each time generating
+    a new number based on the length of the newly created varaible
+    above.
+  */
   for (let i = 0; i < slider; i++) {
     const randomNum = Math.floor(Math.random() * allCharacters.length);
     pass += allCharacters[randomNum]
@@ -73,9 +104,16 @@ const randomPassStretch = function (slider) {
 
 // range slider for mouse input
 rangeSliderEl.addEventListener('mousemove', function () {
-  rangeSliderTextEl.textContent = rangeSliderEl.value;
+  // this updates the slider varaible above
   sliderValue = rangeSliderEl.value
+  // sets the text next to the slider to the range input value
+  rangeSliderTextEl.textContent = sliderValue;
 
+  /*
+    simple if statement,
+    this just controls the size of the text when sliding
+    the slider, not something that is needed, but a nice little touch.
+  */
   if (rangeSliderEl.value < 30) {
     rangeSliderTextEl.style.color = 'var(--clr-dt-grey)';
     rangeSliderTextEl.style.transform = 'scale(1)'
@@ -88,7 +126,10 @@ rangeSliderEl.addEventListener('mousemove', function () {
   }
 })
 
-// range slider for finger/touch input
+/*
+  range slider for finger/touch input, same as above 
+  just for screens/mobile devices
+*/
 rangeSliderEl.addEventListener('touchmove', function () {
   rangeSliderTextEl.textContent = rangeSliderEl.value;
   sliderValue = rangeSliderEl.value
@@ -105,9 +146,19 @@ rangeSliderEl.addEventListener('touchmove', function () {
   }
 })
 
-// toggle switches
+/*
+  Looping over the toggle buttons, as I selected all of
+  them in a node list/array, instead of selecting them one
+  by one.
+*/
 for (let i = 0; i < toggleBtnsEl.length; i++) {
   toggleBtnsEl[i].addEventListener('click', function (evt) {
+    /*
+      I am using evt.target to select the element the pointer is actively clicking. Im looking to see the state of the checkbox.
+
+      it adds a css class as well as goes deeper and sets the 
+      toggle varaibles above for use within the random function.
+    */
     if (evt.target.checked) {
       toggleFillsEl[i].classList.add('toggle-active')
       toggleTextsEl[i].classList.add('toggle-active')
@@ -133,20 +184,25 @@ for (let i = 0; i < toggleBtnsEl.length; i++) {
   })
 }
 
+/*
+  generates the passwords, just loops over the text areas 
+  and calls the random function.
+*/
 generateStretchBtnEl.addEventListener('click', function () {
-  const passwords = [
-    randomPassStretch(sliderValue),
-    randomPassStretch(sliderValue)
-  ]
-
   for (let i = 0; i < displayStretchTextsEl.length; i++) {
-    displayStretchTextsEl[i].textContent = passwords[i];
+    displayStretchTextsEl[i].textContent = randomPassStretch(sliderValue);
   }
 })
 
-// copy password
+/*
+  copy password on btn click
+*/
 for (let i = 0; i < btnsCopyEl.length; i++) {
   btnsCopyEl[i].addEventListener('click', function () {
+
+    /*
+      small note on 
+    */
     let copy = displayStretchTextsEl[i].textContent;
     navigator.clipboard.writeText(copy)
     const message = setInterval(function () {
